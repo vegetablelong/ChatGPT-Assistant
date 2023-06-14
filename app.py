f6433015f068005d0585ac59ad238606bd6019de
vegetablelong <68897296+vegetablelong@users.noreply.github.com>
2023-06-14 14:26:33 +0800

    Update app.py
    logout modify

--- app.py
+++ app.py
@@ -60,6 +60,8 @@ if authentication_status:
 
 
     with st.sidebar:
+        # logout
+        authenticator.logout("Logout", "siderbar")
         st.markdown("# 🤖 聊天窗口")
         # 创建容器的目的是配合自定义组件的监听操作
         chat_container = st.container()
